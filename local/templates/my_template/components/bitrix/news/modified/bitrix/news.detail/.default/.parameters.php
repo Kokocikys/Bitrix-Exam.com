5b50fb030ea167a0c681ebbe91e3d68946284833
kokocikys <chernecky_ilia@rambler.ru>
2021-02-17 17:17:41 +0300

    Component modified

--- local/templates/my_template/components/bitrix/news/modified/bitrix/news.detail/.default/.parameters.php
+++ local/templates/my_template/components/bitrix/news/modified/bitrix/news.detail/.default/.parameters.php
@@ -1,6 +1,17 @@
 <?
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
+if (!CModule::IncludeModule("iblock"))
+    return;
+
+$arIBlockType = CIBlockParameters::GetIBlockTypes();
+
+$arIBlock = array();
+$rsIBlock = CIBlock::GetList(array("SORT" => "ASC"), array("TYPE" => $arCurrentValues["IBLOCK_TYPE"], "ACTIVE" => "Y"));
+while ($arr = $rsIBlock->Fetch()) {
+    $arIBlock[$arr["ID"]] = "[" . $arr["ID"] . "] " . $arr["NAME"];
+}
+
 $arTemplateParameters = array(
     "DISPLAY_DATE" => array(
         "NAME" => GetMessage("T_IBLOCK_DESC_NEWS_DATE"),
@@ -24,7 +35,29 @@ $arTemplateParameters = array(
     ),
     "CANONICAL" => array(
         "NAME" => "CANONICAL",
-        "TYPE" => "STRING",
+        "TYPE" => "CHECKBOX",
+        "DEFAULT" => "N",
     ),
 );
+
+if ($arCurrentValues["CANONICAL"] == "Y") {
+    $arTemplateParameters = array(
+        "CANONICAL" => array(
+            "NAME" => "CANONICAL",
+            "TYPE" => "CHECKBOX",
+            "DEFAULT" => "N",
+            "REFRESH" => "Y",
+        ),
+        "IBLOCK_TYPE_CANONICAL" => array(
+            "NAME" => "Тип инфоблока",
+            "TYPE" => "LIST",
+            "VALUES" => $arIBlockType,
+        ),
+        "IBLOCK_ID_CANONICAL" => array(
+            "NAME" => "Инфоблок",
+            "TYPE" => "LIST",
+            "VALUES" => $arIBlock,
+        )
+    );
+}
 ?>
