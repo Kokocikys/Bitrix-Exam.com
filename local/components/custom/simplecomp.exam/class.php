40b6190df8ad1b5692f0ee14228e1a3e69c50824
kokocikys <chernecky_ilia@rambler.ru>
2021-02-20 18:45:15 +0300

    Ex2_70 done

--- local/components/custom/simplecomp.exam/class.php
+++ local/components/custom/simplecomp.exam/class.php
@@ -3,67 +3,63 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
 class TestComponent extends CBitrixComponent
 {
-    public function newsWithGroupedSections()
+    public function newsIDsWithTheirSections()
     {
-        $arNews = array();
-
         $arFilter = array('IBLOCK_ID' => $this->arParams["IBLOCK_ID_CATALOG"], "ACTIVE" => "Y");
-        $arSelect = array("ID", "NAME", "UF_*");
+        $arSelect = array("ID", "NAME", $this->arParams["USER_SECTION_PROP_CODE"]);
         $sections = CIBlockSection::GetList(array(), $arFilter, false, $arSelect);
 
         while ($section = $sections->Fetch()) {
             foreach ($section[$this->arParams["USER_SECTION_PROP_CODE"]] as $newsID) {
-                if (array_key_exists($newsID, $arNews)) {
-                    array_push($arNews[$newsID]["SECTION_IDs"], $section["ID"]);
-                    array_push($arNews[$newsID]["SECTION_NAMEs"], $section["NAME"]);
+                if (array_key_exists($newsID, $this->arResult["NEWS"])) {
+                    array_push($this->arResult["NEWS"][$newsID]["SECTION_IDS"], $section["ID"]);
+                    array_push($this->arResult["NEWS"][$newsID]["SECTION_NAMES"], $section["NAME"]);
                 } else {
-                    $arNews[$newsID]["SECTION_IDs"] = array($section["ID"]);
-                    $arNews[$newsID]["SECTION_NAMEs"] = array($section["NAME"]);
+                    $this->arResult["NEWS"][$newsID]["SECTION_IDS"] = array($section["ID"]);
+                    $this->arResult["NEWS"][$newsID]["SECTION_NAMES"] = array($section["NAME"]);
                 }
             }
         }
-        return $arNews;
     }
 
     public function productsGroupedByNews()
     {
-        $result = array();
-        $newsWithGroupedSections = $this->newsWithGroupedSections();
-
-        foreach ($newsWithGroupedSections as $newsID => $sectionsIDs) {
+        foreach ($this->arResult["NEWS"] as $newsID => $sectionsIDs) {
 
             $arFilter = array('IBLOCK_ID' => $this->arParams["IBLOCK_ID_NEWS"], "ID" => $newsID, "ACTIVE" => "Y");
             $arSelect = array("NAME", "DATE_ACTIVE_FROM");
             $singleNews = CIBlockElement::GetList(array(), $arFilter, false, false, $arSelect);
 
             $newsValues = $singleNews->Fetch();
-            $result[$newsValues["NAME"]]["NEWS_VALUES"] = $newsValues;
-            $result[$newsValues["NAME"]]["NEWS_VALUES"]["SECTION_NAMES"] = $newsWithGroupedSections[$newsID]["SECTION_NAMEs"];
+            $this->arResult["RESULT"][$newsValues["NAME"]]["NEWS_VALUES"] = $newsValues;
+            $this->arResult["RESULT"][$newsValues["NAME"]]["NEWS_VALUES"]["SECTION_NAMES"] = $this->arResult["NEWS"][$newsID]["SECTION_NAMES"];
 
-            $arFilter = array('IBLOCK_ID' => $this->arParams["IBLOCK_ID_CATALOG"], "ACTIVE" => "Y"); //'SECTION_ID' => $sectionsID,
-            $arSelect = array("ID", "IBLOCK_SECTION_ID", 'NAME', "PROPERTY_PRICE", "PROPERTY_MATERIAL", "PROPERTY_ARTNUMBER"); //
-            $elements = CIBlockElement::GetList(array(), $arFilter, false, false, $arSelect);
+            foreach ($sectionsIDs["SECTION_IDS"] as $sectionsID) {
 
-            while ($product = $elements->Fetch()) {
-                $counter++;
-                foreach ($sectionsIDs["SECTION_IDs"] as $sectionsID) {
-                    if ($product["IBLOCK_SECTION_ID"] == $sectionsID) {
-                        $result[$newsValues["NAME"]]["PRODUCTS"][$product["NAME"]] = $product;
-                    }
+                $arFilter = array('IBLOCK_ID' => $this->arParams["IBLOCK_ID_CATALOG"], "IBLOCK_SECTION_ID" => $sectionsID, "ACTIVE" => "Y");
+                $arSelect = array("ID", "IBLOCK_SECTION_ID", 'NAME', "PROPERTY_PRICE", "PROPERTY_MATERIAL", "PROPERTY_ARTNUMBER"); //
+                $elements = CIBlockElement::GetList(array(), $arFilter, false, false, $arSelect);
+
+                while ($product = $elements->Fetch()) {
+                    $this->arResult["RESULT"][$newsValues["NAME"]]["PRODUCTS"][$product["NAME"]] = $product;
                 }
             }
-            $this->arResult["COUNTER"] = $counter;
-            $counter = 0;
         }
-        return $result;
+    }
+
+    public function countElements()
+    {
+        $numberOfElements = CIBlockElement::GetList(array(), array('IBLOCK_ID' => $this->arParams["IBLOCK_ID_CATALOG"]), array(), false, array());
+        return $numberOfElements;
     }
 
     public function executeComponent()
     {
-
-        $this->arResult["RESULT"] = $this->productsGroupedByNews();
+        $this->newsIDsWithTheirSections();
+        $this->productsGroupedByNews();
         $this->IncludeComponentTemplate();
+
         global $APPLICATION;
-        $APPLICATION->SetTitle("В каталоге товаров представлено товаров: " . $this->arResult['COUNTER']);
+        $APPLICATION->SetTitle("В каталоге товаров представлено товаров: " . $this->countElements());
     }
 }
