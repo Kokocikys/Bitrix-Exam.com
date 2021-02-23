2ac71e10779a52a4e3a86e3dd903a332d85bfa77
kokocikys <chernecky_ilia@rambler.ru>
2021-02-23 16:49:35 +0300

    Ex2-82 quick fix

--- local/components/custom/simplecomp.exam/templates/.default/component_epilog.php
+++ local/components/custom/simplecomp.exam/templates/.default/component_epilog.php
@@ -1,13 +1,6 @@
 <?php
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
-echo '<pre>';
-var_dump($arResult["MIN_PRICE"]);
-echo '</pre>';
-echo '<pre>';
-var_dump($arResult["MAX_PRICE"]);
-echo '</pre>';
-
 if (isset($arResult["MIN_PRICE"]) && isset($arResult["MAX_PRICE"])) {
 
     $content = "Минимальная цена: " . $arResult["MIN_PRICE"] . "<br>" . "Максимальная цена: " . $arResult["MAX_PRICE"];
