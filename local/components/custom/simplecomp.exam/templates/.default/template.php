eaad55dea7f85819ed68e9a651385254158cc166
kokocikys <chernecky_ilia@rambler.ru>
2021-02-23 17:08:19 +0300

    Ex2-82 content output removed in template.php + component_epilog.php deleted

--- local/components/custom/simplecomp.exam/templates/.default/template.php
+++ local/components/custom/simplecomp.exam/templates/.default/template.php
@@ -1,17 +1,27 @@
 <?php
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 
-<div>
-    <?php foreach ($arResult["NEWS"] as $values) { ?>
-        <?= $values["NAME"] . " - " . $values["DATE_ACTIVE_FROM"]; ?>
-        <div>(<?= implode(', ', $arResult["SECTIONS"][$values["ID"]]["SECTION_NAMES"]); ?>)
-            <ul>
-                <? foreach ($arResult["PRODUCTS"][$values["ID"]] as $product) { ?>
-                    <li>
-                        <?= $product["NAME"] . " - " . $product["PROPERTY_PRICE_VALUE"] . " - " . $product["PROPERTY_MATERIAL_VALUE"] . " - " . $product["PROPERTY_ARTNUMBER_VALUE"]; ?>
-                    </li>
-                <? } ?>
-            </ul>
-        </div>
-    <? } ?>
-</div>
+    <div>
+        <?php foreach ($arResult["NEWS"] as $values) { ?>
+            <?= $values["NAME"] . " - " . $values["DATE_ACTIVE_FROM"]; ?>
+            <div>(<?= implode(', ', $arResult["SECTIONS"][$values["ID"]]["SECTION_NAMES"]); ?>)
+                <ul>
+                    <? foreach ($arResult["PRODUCTS"][$values["ID"]] as $product) { ?>
+                        <li>
+                            <?= $product["NAME"] . " - " . $product["PROPERTY_PRICE_VALUE"] . " - " . $product["PROPERTY_MATERIAL_VALUE"] . " - " . $product["PROPERTY_ARTNUMBER_VALUE"]; ?>
+                        </li>
+                    <? } ?>
+                </ul>
+            </div>
+        <? } ?>
+    </div>
+
+<? $this->SetViewTarget('ex2-82'); ?>
+    <div style="color:red; margin: 34px 15px 35px 15px">
+        <? if (isset($arResult["MIN_PRICE"])) ?>
+        <?= "Минимальная цена: " . $arResult["MIN_PRICE"]; ?>
+        <br>
+        <? if (isset($arResult["MAX_PRICE"])) ?>
+        <?= "Максимальная цена: " . $arResult["MAX_PRICE"]; ?>
+    </div>
+<? $this->EndViewTarget(); ?>
