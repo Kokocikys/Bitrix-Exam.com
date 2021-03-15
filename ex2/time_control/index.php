51de002cb4ec9d5f168b29dfbd8b6b8f0dd61eef
kokocikys <chernecky_ilia@rambler.ru>
2021-03-15 17:58:28 +0300

    'WIP'

--- ex2/time_control/index.php
+++ ex2/time_control/index.php
@@ -1,7 +1,16 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Оценка производительности");
-?>Самая ресурсоемкая страница –&nbsp;&nbsp;/products/index.php.<br>
+?>_____Ex2-11<br>
+ <br>
+ Самая ресурсоемкая страница –&nbsp;&nbsp;/products/index.php.<br>
  Нагрузка 25.06%, среднее "Страница-Время" – 2.2295 с.<br>
-Проблемный компонент:&nbsp;bitrix:catalog.section.<br>
-Занимает&nbsp;<nobr>0.2015 с и делает з</nobr>апросов: 34<br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+ Проблемный компонент:&nbsp;bitrix:catalog.section.<br>
+ Занимает&nbsp;<nobr>0.2015 с и делает з</nobr>апросов: 34<br>
+ <br>
+ _____Ex2-10<br>
+ <br>
+ Самая ресурсоемкая страница –&nbsp;&nbsp;<br>
+ Нагрузка , среднее "Страница-Время" –&nbsp; с.<br>
+ Проблемный компонент: .<br>
+ Занимает&nbsp;<nobr>&nbsp;с и делает з</nobr>апросов:&nbsp;<br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
